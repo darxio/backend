6debd4816de309553783873a4091bdf95b42f112
ksgl <ksenia.glazacheva@gmail.com>
2019-09-27 22:00:53 +0300

    db connection

---
+++ internal/database/database.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"sync"
+
+	"github.com/jackc/pgx"
+)
+
+var DB *pgx.ConnPool
+var once sync.Once
+
+const maxConn = 8
+
+func Connect() (conn *pgx.ConnPool) {
+	once.Do(func() {
+		connConfig := pgx.ConnConfig{
+			User:     "ksu",
+			Password: "pswd",
+			Host:     "localhost",
+			Port:     5432,
+			Database: "backend_db",
+		}
+
+		DB, _ = pgx.NewConnPool(pgx.ConnPoolConfig{
+			ConnConfig:     connConfig,
+			MaxConnections: maxConn,
+		})
+	})
+
+	return DB
+}
