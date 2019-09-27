fb0bfdbbb5cdbefb55ece3200a8988fe556b53ec
ksgl <ksenia.glazacheva@gmail.com>
2019-09-28 00:47:43 +0300

    router

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+
+	"github.com/buaazp/fasthttprouter"
+	"github.com/valyala/fasthttp"
+
+	"backend/internal/api/userapi"
+	mw "backend/internal/middleware"
+)
+
 func main() {
-	return
+	r := fasthttprouter.New()
+
+	r.POST("/users", userapi.SignUp)
+	r.POST("/session", userapi.SignIn)
+	r.DELETE("/session", mw.Auth(userapi.SignOut))
+
+	log.Println("Listening on http://localhost:8888...")
+	fasthttp.ListenAndServe(":8888", r.Handler)
 }
