6bf61c19e7ac0da84c51de30f5608d5b16c50128
Artem <mouseartiom@yandex.ru>
2019-11-18 00:41:03 +0300

    changed db model

--- internal/database/groups/groups.go
+++ internal/database/groups/groups.go
@@ -56,7 +56,7 @@ func Ingredients(groupID int, count int, offset int, ingredients *models.Ingredi
 	rows, err := database.Query(`
 		SELECT id, name, danger, description, wiki_link 
 			FROM ingredients WHERE id IN (
-				SELECT id FROM ing_groups WHERE (NOT ($1 = ANY (groups))) AND (array_length(groups, 1) > 0)
+				SELECT id FROM ing_groups WHERE  ($1 = ANY (groups))
 			)
 				ORDER BY frequency DESC, danger DESC LIMIT $2 OFFSET $3
 				`, strconv.Itoa(groupID), count, offset)
